b55b9b4a6aaaf0bace9bc959404aba38f123da4a
towhid7667 <towhidulhq@gmail.com>
2024-03-13 03:44:30 +0600

    max path sum

---
+++ tree/max_path_sum.py
@@ -0,0 +1,8 @@
+def max_path_sum(root):
+  if root is None:
+    return float('-inf')
+  if root.left is None and root.right is None:
+    return root.val
+  max_left = max_path_sum(root.left)
+  max_right = max_path_sum(root.right) 
+  return root.val + max(max_left, max_right)
